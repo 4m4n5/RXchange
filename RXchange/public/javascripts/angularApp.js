f98f311f49f215332ca5aee4f36f773fd17f2a3a
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-08 20:36:21 +0530

    Created frontend for login and auth

--- RXchange/public/javascripts/angularApp.js
+++ RXchange/public/javascripts/angularApp.js
@@ -35,6 +35,51 @@ app.config([
   }
 ]);
 
+app.factory('auth', ['$http', '$window',
+  function($http, $window) {
+    var auth = {};
+    auth.saveToken = function(token) {
+      $window.localStorage['rxchange-token'] = token;
+    };
+    auth.getToken = function() {
+      return $window.localStorage['rxchange-token'];
+    };
+    auth.isLoggedIn = function() {
+      var token = auth.getToken();
+
+      if (token) {
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+    auth.currentUser = function() {
+      if (auth.isLoggedIn()) {
+        var token = auth.getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.username;
+      }
+    };
+    auth.register = function(user) {
+      return $http.post('/register', user).success(function(data) {
+        auth.saveToken(data.token);
+      });
+    };
+    auth.logIn = function(user) {
+      return $http.post('/login', user).success(function(data) {
+        auth.saveToken(data.token);
+      });
+    };
+    auth.logOut = function() {
+      $window.localStorage.removeItem('flapper-news-token');
+    };
+    return auth;
+  }
+]);
+
 app.factory('posts', ['$http',
 
   function($http) {
@@ -162,6 +207,31 @@ app.controller('PostsCtrl', [
         $scope.post.comments.push(comment);
       });
       $scope.body = '';
+    };
+  }
+]);
+
+app.controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'auth',
+  function($scope, $state, auth) {
+    $scope.user = {};
+
+    $scope.register = function() {
+      auth.register($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
+    };
+
+    $scope.logIn = function() {
+      auth.logIn($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
     };
   }
 ]);
