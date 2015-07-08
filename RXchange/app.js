1cb5a3701ea355efa8dcf1123a0724e0b43286fc
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-08 14:40:53 +0530

    Added db connection

--- RXchange/app.js
+++ RXchange/app.js
@@ -5,10 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// connect to db
+mongoose.connect('mongodb://localhost/rxchange_test');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +23,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
