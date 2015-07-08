2f2e0fca4e3e6d6b8f83c42432ce56a11035d402
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-08 15:04:12 +0530

    Added route to get all posts

--- RXchange/routes/index.js
+++ RXchange/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(posts);
+  });
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
