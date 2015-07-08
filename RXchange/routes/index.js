706eb34084d5276ca1226e991500cc226b833c46
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-08 20:19:02 +0530

    Added routes for login and authentication

--- RXchange/routes/index.js
+++ RXchange/routes/index.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+var passport = require('passport');
+var User = mongoose.model('User');
+
 // to get all posts
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
@@ -115,6 +118,54 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 
     res.json(comment);
   });
+});
+
+// creates user
+router.post('/register', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: 'Please fill out all fields'
+    });
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    if (err) {
+      return next(err);
+    }
+
+    return res.json({
+      token: user.generateJWT()
+    });
+  });
+});
+
+// login route to authenticate user
+router.post('/login', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: 'Please fill out all fields'
+    });
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      return res.json({
+        token: user.generateJWT()
+      });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 /* GET home page. */
