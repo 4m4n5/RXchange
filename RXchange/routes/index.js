9e2bf0713b3b316a4e01aac016d54be7f13dd65a
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-08 15:27:58 +0530

    Added route to get indivisual posts

--- RXchange/routes/index.js
+++ RXchange/routes/index.js
@@ -29,8 +29,17 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+// to get a singlr post
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
 
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
 
+    req.post = post;
+    return next();
+});});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
