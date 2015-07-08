8fb1c472618518fb6e0fcff234c11d326e25ed56
Aman Shrivastava <aman.srivastava999@gmail.com>
2015-07-08 15:06:27 +0530

    Added route to post a new post

--- RXchange/routes/index.js
+++ RXchange/routes/index.js
@@ -15,6 +15,18 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(post);
+  });
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
