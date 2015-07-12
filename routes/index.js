406fe0fac592ad442549e75d9856708d558c2436
Sahil Lamba <sahil.lamba95@gmail.com>
2015-07-12 15:12:53 +0530

    Login and sign up ready

--- routes/index.js
+++ routes/index.js
@@ -156,7 +156,7 @@ router.post('/register', function(req, res, next) {
 router.post('/login', function(req, res, next) {
   if (!req.body.username || !req.body.password) {
     return res.status(400).json({
-      message: 'Please fill out all fields'
+      message: 'Please fill out all fields.'
     });
   }
 
